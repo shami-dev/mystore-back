4de78dbdc947fa1c91cbe539d32672d594b096cf
Shamil Khaibullov <shamil@shamil.dev>
2025-09-30 15:26:28 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express!");
+});
+
+export { app };
